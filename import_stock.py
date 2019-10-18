068ff367204f35314e9e0fff44b932ed72040013
毛越 <maoyue@maoyuedembp.home>
2019-10-18 10:54:39 -0400

    remove useless code

--- import_stock.py
+++ import_stock.py
@@ -13,6 +13,32 @@ def get_file(path):
                     yield os.path.join(os.path.join(path, dir), file)
 
 
+def get_symbol_list():
+    symbols = []
+    for f in get_file("./stock-data/Data/"):
+        symbols.append(f.split("/")[-1].split(".")[0])
+    return symbols
+
+
+def check_name():
+    symbols = set(get_symbol_list())
+    data = pd.read_csv("./stock-data/companylist.csv")
+    valid_symbols = set([s.lower() for s in data["Symbol"].tolist()])
+    return symbols.intersection(valid_symbols), data
+
+
+def make_symbol_name_mapping():
+    symbols, data = check_name()
+    res = {'Symbol': [], 'Name': []}
+    for symbol in symbols:
+        name = data[data['Symbol'] == symbol.upper()]['Name'].values[0]
+        print(name)
+        res['Symbol'].append(symbol)
+        res['Name'].append(name)
+    res = pd.DataFrame(res)
+    res.to_csv("./stock-data/symbol-name.csv", index = False)
+
+
 def import_stock(cur):
     print("---------------importing stock---------------")
     names = pd.read_csv("./stock-data/symbol-name.csv")
@@ -44,71 +70,24 @@ def import_stock_history(cur):
                 spid += 1
 
 
-def import_rec_stock(cur):
-    print("---------------importing stock history price---------------")
-    data = pd.read_csv("./rec_stock.csv")
-    for i, row in data.iterrows():
-        sql = """INSERT INTO rec_stk(aid, sid) VALUES ('%s', '%s')""" % (row['Aid'], row['Sid'])
-        cur.execute(sql)
-
-
-def get_symbol_list():
-    symbols = []
-    for f in get_file("./stock-data/Data/"):
-        symbols.append(f.split("/")[-1].split(".")[0])
-    return symbols
-
-
-def check_name():
-    symbols = set(get_symbol_list())
-    data = pd.read_csv("./stock-data/companylist.csv")
-    valid_symbols = set([s.lower() for s in data["Symbol"].tolist()])
-    return symbols.intersection(valid_symbols), data
-
-
-def make_symbol_name_mapping():
-    symbols, data = check_name()
-    res = {'Symbol': [], 'Name': []}
-    for symbol in symbols:
-        name = data[data['Symbol'] == symbol.upper()]['Name'].values[0]
-        print(name)
-        res['Symbol'].append(symbol)
-        res['Name'].append(name)
-    res = pd.DataFrame(res)
-    res.to_csv("./stock-data/symbol-name.csv", index = False)
-
-
 def import_stock_data():
     conn = db.connect(name = "proj1part2", usr = "yl4323", host = "35.243.220.243", pwd = "2262")
     if conn is None:
         exit(1)
     else:
         cur = conn.cursor()
-        # import_stock(cur)
-        # conn.commit()
+
+        cur.execute("DELETE FROM stocks;")
+        cur.execute("DELETE FROM stock_history;")
+
+        import_stock(cur)
+        conn.commit()
 
         import_stock_history(cur)
         conn.commit()
-
-
-def check_data():
-    valid_symbols = set(pd.read_csv("./stock-data/symbol-name.csv")['Symbol'].values)
-    for f in get_file("./stock-data/Data/"):
-        symbol = f.strip().split("/")[-1].split(".")[0]
-        if symbol in valid_symbols:
-            cnt = open(f, 'r').read()
-            if len(cnt) == 0:
-                continue
-            else:
-                data = pd.read_csv(f)
             
 
 if __name__ == '__main__':
-    conn = db.connect(name = "proj1part2", usr = "yl4323", host = "35.243.220.243", pwd = "2262")
-    if conn is None:
-        exit(1)
-    else:
-        cur = conn.cursor()
-        import_rec_stock(cur)
-        conn.commit()
+    make_symbol_name_mapping()
+    import_stock_data()
 
